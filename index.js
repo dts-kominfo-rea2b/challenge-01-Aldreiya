0565d2e2194b771e2aeb3bb8b0bb0453146c9927
Aldreiya Stier <aldreiya99@gmail.com>
2022-05-29 21:10:05 +0700

    modify some data type

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+
 const color1 = new Set(['Yellow', 'Pink', 'White', 'Purple']);
 const color2 = new Set(['Blue', 'Black', 'Grey']);
 const restaurant1 = new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']);
 const restaurant2 = new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']);
+
 const firstUser = {
     name: 'Monica',
     gender: 'Female',
-    age: '17',
+    age: 17,
     email: 'monica@dingdong.com',
-    favoriteColor: color1,
-    isHavePet: 'Yes',
+    favoriteColor: Array.from(color1),
+    isHavePet: true,
     education: [
         {name: 'SD 01', city: 'Jakarta', graduate: '2016'},
         {name: 'SMP 02', city: 'Jakarta', graduate: '2019'},
         {name: 'SMA 03', city: 'Tangerang'}
     ],
-    favoriteRestaurant: restaurant1
+    favoriteRestaurant: Array.from(restaurant1)
 };
 const secondUser = {
     name: 'Wendy',
     gender: 'Male',
-    age: '23',
+    age: 23,
     email: 'wendy@dingdong.com',
-    favoriteColor: color2,
-    isHavePet: 'No',
+    favoriteColor: Array.from(color2),
+    isHavePet: false,
     education: [
         {name: 'SD 02', city: 'Jakarta', graduate: '2010'},
         {name: 'SMP 03', city: 'Bogor', graduate: '2013'},
         {name: 'SMA 01', city: 'Surabaya', graduate: '2016'},
         {name: 'Universitas Maju', city: 'Tangerang'},
     ],
-    favoriteRestaurant: restaurant2
+    favoriteRestaurant: Array.from(restaurant2)
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [firstUser, secondUser];
+const users = [];
+
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
